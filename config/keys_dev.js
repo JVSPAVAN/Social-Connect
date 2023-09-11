98c3b64cd462e3d207eb87b06b83e6cc9c0dbdbe
Vinay V <jvspk.1996@gmail.com>
2023-09-11 14:20:29 -0400

    set default database

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-     mongoURI: "mongodb+srv://saiii:jvs<3gag@clustersaiii.iacxx.mongodb.net/?retryWrites=true&w=majority",
-     secretOrKey: 'secret'
+     mongoURI: "mongodb+srv://saiii:jvs<3gag@clustersaiii.iacxx.mongodb.net/stores?retryWrites=true&w=majority",
+     secretOrKey: 'secretOrKey'
    };
