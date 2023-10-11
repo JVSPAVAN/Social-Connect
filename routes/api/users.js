5357b5ccd6cdea5b2b9865e933e554fb2f45272c
Vinay V <jvspk.1996@gmail.com>
2023-10-11 00:51:54 -0500

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -86,6 +86,7 @@ router.post("/login", (req, res) => {
           keys.secretOrKey,
           { expiresIn: 3600 },
           (err, token) => {
+            console.log("tiken ",token);
             res.json({
               success: true,
               token: "Bearer " + token
